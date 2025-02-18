18d9a2fb1d880c8abf8639e56d7c221a96f01ffc
Proteen Das <proteen.das@mindsprint.org>
2025-02-18 16:07:35 +0530

    JS Fix

---
+++ blocks/text-image/text-image.js
@@ -0,0 +1,22 @@
+export default function decorate() {
+  const textImageWrapper = document.querySelector('.text-image-wrapper');
+  const imageDiv = textImageWrapper.querySelector('picture');
+  const headingDiv = textImageWrapper.querySelector('h4');
+  const descriptionDiv = textImageWrapper.querySelector('p');
+  const textContainer = document.createElement('div');
+  textContainer.classList.add('text-content');
+  if (headingDiv) {
+    textContainer.appendChild(headingDiv);
+  }
+  if (descriptionDiv) {
+    textContainer.appendChild(descriptionDiv);
+  }
+  textImageWrapper.innerHTML = '';
+  const newStructure = `
+    <div class="image-container">
+        ${imageDiv.outerHTML} <!-- Add the image -->
+    </div>
+    ${textContainer.outerHTML} <!-- Add the text container -->
+    `;
+  textImageWrapper.innerHTML = newStructure;
+}
