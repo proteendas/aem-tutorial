d5535d76a690b4c8bd1d05990554f4d77197def9
Proteen Das <proteen.das@mindsprint.org>
2025-02-20 15:35:14 +0530

    Text Image JS Changes

--- blocks/text-image/text-image.js
+++ blocks/text-image/text-image.js
@@ -1,31 +1,30 @@
 export default function decorate() {
   const textImageWrapper = document.querySelector('.text-image-wrapper');
-  const imageDiv = textImageWrapper.querySelector('picture');
-  const headingDiv = textImageWrapper.querySelector('h4');
-  const descriptionDiv = textImageWrapper.querySelector('p');
-  const buttonDiv = textImageWrapper.querySelector('.button-container');
 
-  const textContainer = document.createElement('div');
-  textContainer.classList.add('text-content');
+  if (textImageWrapper) {
+    // Select the first div containing a picture element and add the class 'image-container'
+    const imageDiv = textImageWrapper.querySelector('div > picture')?.closest('div');
+    if (imageDiv) {
+      imageDiv.classList.add('image-container');
+    }
 
-  if (headingDiv) {
-    textContainer.appendChild(headingDiv);
+    // Create a new div with the class 'text-container'
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('text-container');
+
+    // Select all the divs that need to be placed inside the 'text-container'
+    const textDivs = textImageWrapper.querySelectorAll('div[data-aue-prop="text"], div[data-aue-prop="description"], div[data-aue-model="button"]');
+
+    // Append each selected div to the new 'text-container'
+    textDivs.forEach(div => {
+      textContainer.appendChild(div);
+    });
+
+    // Insert the 'text-container' after the 'image-container'
+    if (imageDiv) {
+      imageDiv.parentNode.insertBefore(textContainer, imageDiv.nextSibling);
+    } else {
+      textImageWrapper.appendChild(textContainer);
+    }
   }
-  if (descriptionDiv) {
-    textContainer.appendChild(descriptionDiv);
-  }
-  if (buttonDiv) {
-    textContainer.appendChild(buttonDiv);
-  }
-
-  textImageWrapper.innerHTML = '';
-
-  const newStructure = `
-    <div class="image-container">
-        ${imageDiv.outerHTML}
-    </div>
-    ${textContainer.outerHTML}
-  `;
-
-  textImageWrapper.innerHTML = newStructure;
 }
